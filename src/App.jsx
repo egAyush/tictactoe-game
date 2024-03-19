ea85cdaaf66be6f1fba3e6b6bd4755375021f2d6
egAyush <ayushpandey3269@gmail.com>
2024-03-19 11:02:30 +0530

    added bg balls

--- src/App.jsx
+++ src/App.jsx
@@ -86,6 +86,8 @@ function App() {
         Current Game History
       </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+
+      <div className="bg-balls"></div>
     </div>
   );
 }
