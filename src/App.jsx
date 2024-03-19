94fe4a85e1e2316144aa29567997fdeae334451c
egAyush <ayushpandey3269@gmail.com>
2024-03-19 10:38:44 +0530

    added title and include Roboto font

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,9 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -75,7 +78,13 @@ function App() {
       >
         start New Game
       </button>
-      <h2>Current Game History</h2>
+      <h2
+        style={{
+          fontWeight: 'normal',
+        }}
+      >
+        Current Game History
+      </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
